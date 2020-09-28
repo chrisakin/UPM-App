a46971000fd600ae940bc10c2d369a9ca058f45c
Christopher Akintade <chrisfemide@gmail.com>
2020-09-28 12:34:07 +0100

    url path

--- index.js
+++ index.js
@@ -5,9 +5,22 @@
 // Dependecies
 
 var http = require('http');
+var url = require('url');
 // The server should respond to all request with a string
 var server = http.createServer(function(req, res){
+
+    //Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //Send the response
     res.end('Hello World\n');
+
+    //Log the request path
+    console.log('Request is recieved on path: '+ trimmedPath);
 });
 
 // Start the server and listen to port 3000
